a84956e2fd0a806468b1c7df6ac7e18e8fb3d2e0
Dmitriy <xburv@mail.ru>
2022-10-31 19:54:07 +0500

    Swift

--- Swift_09_Оператор Switch.playground/Contents.swift
+++ Swift_09_Оператор Switch.playground/Contents.swift
@@ -13,9 +13,9 @@ var symbols = 0
 
 for i in string {
     switch i {
-        case "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y" : vowels += 1
-        case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z" : consonants += 1
-        case "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" : numbers += 1
+    case "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y" : vowels += 1
+    case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z" : consonants += 1
+    case "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" : numbers += 1
     default:
         symbols += 1
     }
@@ -29,10 +29,10 @@ for i in string {
 let age = 100
 
 switch age {
-    case 0...13 : print("Childhood")
-    case 14...18 : print("Teenager")
-    case 19...60 : print("Adult")
-    case 61...120 : print("Old")
+case 0...13 : print("Childhood")
+case 14...18 : print("Teenager")
+case 19...60 : print("Adult")
+case 61...120 : print("Old")
 default:
     print("Error! Check the age!")
 }
@@ -41,19 +41,30 @@ default:
 3. У вас есть имя отчество и фамилия студента (русские буквы). Имя начинается с А или О, то обращайтесь к студенту по имени, если же нет, то если у него отчество начинается на В или Д, то обращайтесь к нему по имени и отчеству, если же опять нет, то в случае если фамилия начинается с Е или З, то обращайтесь к нему только по фамилии. В противном случае обращайтесь к нему по полному имени.
 */
 
+let fio = (name : "Петр", patronymic : "Иванович", surname : "Петров")
+
+switch fio {
+case (let name,_,_) where name.hasPrefix("А") || name.hasPrefix("О") : print("Здравствуйте, \(fio.name)!")
+case (_, let patronymic, _) where patronymic.hasPrefix("В") || patronymic.hasPrefix("Д") : print("Здравствуйте, \(fio.name) \(fio.patronymic)!")
+case (_,_,let surname) where surname.hasPrefix("Е") || surname.hasPrefix("З") : print("Здравствуйте, \(fio.surname)!")
+default:
+    print("Здравствуйте, \(fio.surname) \(fio.name) \(fio.patronymic)!")
+}
+
 let (name, patronymic, surname) = ("Иван", "Петрович", "Вольнов")
 
+// v.2 (Работает, но много вложенностей)
 for i in name {
     switch i {
-        case "А", "О" : print("Здравствуйте, \(name)!")
+    case "А", "О" : print("Здравствуйте, \(name)!")
     default:
         for i in patronymic {
             switch i {
-                case "В", "Д" : print("Здравствуйте, \(name) \(patronymic)!")
+            case "В", "Д" : print("Здравствуйте, \(name) \(patronymic)!")
             default:
                 for i in surname {
                     switch i {
-                        case "Е", "З" : print("Здравствуйте, \(surname)!")
+                    case "Е", "З" : print("Здравствуйте, \(surname)!")
                     default:
                         print("Здравствуйте, \(surname) \(name) \(patronymic)!")
                     }
@@ -70,3 +81,14 @@ for i in name {
 /*
 4. Представьте что вы играете в морской бои и у вас осталось некоторое количество кораблей на поле 10Х10 (можно буквы и цифры, а можно только цифры). Вы должны создать свитч, который примет тюпл с координатой и выдаст один из вариантов: мимо, ранил, убил.
  */
+
+let field = (x: 1...10, y: 1...10)
+let ship = (x: 3, y: 6, w: 0, h: 1)
+let shot = (x: 3, y: 7)
+
+switch shot {
+case (ship.x...ship.x+ship.w, ship.y...ship.y+ship.h) where ship.w > 0 || ship.h > 0: print("Ранил!")
+case (ship.x...ship.x+ship.w, ship.y...ship.y+ship.h) : print("Убил!")
+default:
+    print("Мимо!")
+}
